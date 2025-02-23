8d675765041cb2125dcf65a98f29d9c6358b9cdd
L33Z22L11 <L33Z22L11@gmail.com>
2025-02-23 09:28:37 +0800

    nav: 我在校园→i西邮

--- asset/dialog.js
+++ asset/dialog.js
@@ -6,7 +6,7 @@ let dialog = {
 
 dialog.msgs = {
     联系方式: { content: 'QQ: 2399052066 (同时支持QQ邮箱)' },
-    我在校园: { content: '和微信小程序版功能相同，支持签到但定位精度较低。', icon: 'fa-solid fa-location-dot fa-space', code: 'https://gw.wozaixiaoyuan.com/h5/mobile/basicinfo/index' },
+    i西邮: { content: '和微信小程序版功能相同，支持签到但定位精度较低。', icon: 'fa-solid fa-location-dot fa-space', code: 'https://wzxy.xiyou.edu.cn/h5/mobile/basicinfo/index/login/index' },
     第二课堂: { icon: 'fa-solid fa-book-bookmark fa-space', code: 'https://win.9xueqi.com/' },
     青年大学习: { content: '需要微信登录，所以得在微信内打开或者使用微信扫一扫。', icon: 'fa-solid fa-medal fa-space', code: 'https://h5.sxgqt.org.cn/#/statisticsNews' },
     逸夫楼地图: { content: '学长做的，但是要旋转屏幕方向才能使用。', code: 'https://8.u.h5mc.com/c/7oor/ygju/index.html' },
@@ -19,6 +19,8 @@ dialog.msgs = {
 
 dialog.showMsg = (name) => {
     let item = dialog.msgs[name]
+    if (!item)
+        return console.warn(`dialog.msgs.${name}不存在`)
     dialog.title.innerHTML = `<i class="${item.icon || item.code && "fa-solid fa-qrcode" || "fa-solid fa-comment-dots"} fa-space"></i><span>${name}</span>`
     dialog.content.innerHTML = `${item.content || ''}${item.code ?
         `(<strong><a onclick="dialog.phone('${item.code}')">点击直接打开</a></strong>)` : ''}`
